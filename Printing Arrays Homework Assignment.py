3b5beaa465d618fe2f79c1680aabc15bd8f7742d
sarcasm-lgtm <72361354+sarcasm-lgtm@users.noreply.github.com>
2020-10-04 14:51:02 -0700

    Prints an array
    Loops over an array and prints its content

---
+++ Printing Arrays Homework Assignment.py
@@ -0,0 +1,36 @@
+#I tried doing it, but it said this:
+#['Ahana', 'is', 'my', 'sister']
+#Traceback (most recent call last):
+  #File "/Users/aarohi/Documents/Printing Arrays Homework Assignment.py", line 4, in <module>
+    #v=ahana
+#NameError: name 'ahana' is not defined
+
+array1=["Ahana","is","my","sister"]
+print(array1)
+
+v="Ahana "
+q="is "
+t="my "
+s="sister"
+print(v + q + t + s + ". Noice, right?")
+
+#This is some code that I got from a website
+arr = [2,4,5,7,9]
+print("The Array is : ")
+for i in arr:
+    print(i, end = ' ')
+
+arr2 = ["Ahana","is","my","sister"]
+print("The Array is : ")
+for i in arr:
+    print(i, end = ' ')
+    
+#I tried customizing it
+#(to fit the thing that we were doing)
+#but it printed the same thing.
+#It printed this:
+#['Ahana', 'is', 'my', 'sister']
+#Ahana is my sister . Noice, right?
+#The Array is : 
+#2 4 5 7 9 The Array is : 
+#2 4 5 7 9 
