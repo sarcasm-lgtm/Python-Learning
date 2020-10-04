e225e5fbdb1dd19cb6eae367172604a03c61b824
aarohi joshi <aarohi@aarohis-MacBook.local>
2020-10-04 15:56:09 -0700

    Created a user-defined function

---
+++ User-Defined Function.py
@@ -0,0 +1,9 @@
+#def function_name(argument1, argument2) :
+      #statement_1
+      #statement_2
+
+def print_array(x) :
+      print("This is the array: ",x)
+
+today=[34,39,68,102,39,45,56,91,400]
+print_array(today)
